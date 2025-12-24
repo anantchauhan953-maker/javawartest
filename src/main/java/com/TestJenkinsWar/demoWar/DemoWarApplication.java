743c3e9bc92d710880d03a0a356c7373df0c09e2
anantchauhan953-maker <anantchauhan953@gmail.com>
2025-12-24 16:01:01 +0530

    Initial commit

---
+++ src/main/java/com/TestJenkinsWar/demoWar/DemoWarApplication.java
@@ -0,0 +1,15 @@
+package com.TestJenkinsWar.demoWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(DemoWarApplication.class, args);
+        System.out.println("TestJenkinsApplication War started");
+	}
+
+}
---
+++ src/test/java/com/TestJenkinsWar/demoWar/DemoWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestJenkinsWar.demoWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
