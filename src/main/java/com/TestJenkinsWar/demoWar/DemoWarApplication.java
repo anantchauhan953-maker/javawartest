40348d33cad92712cf12dfc9deace4c8f953ea46
anantchauhan953-maker <anantchauhan953@gmail.com>
2025-12-24 19:42:08 +0530

    TestJenking on Git Repov21

--- src/main/java/com/TestJenkinsWar/demoWar/DemoWarApplication.java
+++ src/main/java/com/TestJenkinsWar/demoWar/DemoWarApplication.java
@@ -2,10 +2,15 @@ package com.TestJenkinsWar.demoWar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoWarApplication {
-
+public class DemoWarApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoWarApplication.class);
+    }
 	public static void main(String[] args) {
 
         SpringApplication.run(DemoWarApplication.class, args);
